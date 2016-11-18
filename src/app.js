c4ebcf151c99b6598632e9a0dcbe8d56432af348
doubleface <mr.thiriot@gmail.com>
2016-11-18 21:52:49 +0100

    test

--- src/app.js
+++ src/app.js
@@ -1,13 +1,4 @@
-cozysdk.defineRequest("doctypes", "getsums", {
-    map(doc) {
-        if (doc.docType != null) {
-            emit("coucou", 1);
-        }
-    },
-    reduce(keys, values, rereduce) {
-        return sum(values);
-    }
-}, () => {
+cozysdk.defineRequest("doctypes", "getsums", 'function(doc) { if (doc.docType != null) { emit(doc.docType, 1); } }', () => {
     cozysdk.queryView("doctypes", "getsums", {group: true}, (err, res) => {
         console.log(err, "err");
         console.log(res, "res");
