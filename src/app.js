4144561d2ccfb5addeceb4e62d1e16c9d6b9fb42
doubleface <mr.thiriot@gmail.com>
2016-11-19 00:06:16 +0100

    now use promises

--- src/app.js
+++ src/app.js
@@ -1,6 +1,35 @@
-cozysdk.defineRequest("doctypes", "getsums", 'function(doc) { if (doc.docType != null) { emit(doc.docType, 1); } }', () => {
-    cozysdk.queryView("doctypes", "getsums", {group: true}, (err, res) => {
-        console.log(err, "err");
-        console.log(res, "res");
+function initDoctypeRequest() {
+    return new Promise((resolve, reject) => {
+        cozysdk.client.put("request/doctypes/getsums", {
+            map: 'function(doc) { if (doc.docType != null) { emit(doc.docType, 1); } }',
+            reduce: 'function(keys, values, rereduce){return sum(values)}'
+        }, (err, res, body) => {
+            if (err) reject(err);
+            else resolve(body);
+        });
     });
+}
+
+function getDoctypes() {
+    return new Promise((resolve, reject) => {
+        cozysdk.queryView("doctypes", "getsums", {group: true})
+        .then(result => {
+            resolve(result);
+        })
+        .catch(err => {
+            initDoctypeRequest().then(() => {
+                cozysdk.queryView("doctypes", "getsums", {group: true})
+                then(result => {
+                    resolve(result);
+                })
+                .catch(err => {
+                    reject(err);
+                });
+            });
+        });
+    });
+}
+
+getDoctypes().then(result => {
+    console.log(result, "result");
 });
